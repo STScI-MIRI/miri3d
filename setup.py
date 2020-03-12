e37bbb89e45307a50cb5e519444d87c96addd003
David R. Law <dlaw@stsci.edu>
2020-03-12 10:17:12 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='miri3d',
+   version='1.0.0',
+   description='MIRI 3d cube tools',
+   author='David R. Law',
+   author_email='dlaw@stsci.edu',
+   packages=['miri3d'],
+   install_requires=['miricoord'],
+)
