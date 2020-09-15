7d3b4332f282ebcf13a61c7e5158d75cc1325ecf
David Law <dlaw@stsci.edu>
2020-09-15 14:37:14 -0400

    Fix installation for regular install

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from glob import glob
 
 setup(
    name='miri3d',
@@ -7,5 +8,9 @@ setup(
    author='David R. Law',
    author_email='dlaw@stsci.edu',
    packages=find_packages(),
+   data_files=[
+       ('data/lvl2btemplate',glob('data/lvl2btemplate/*'))
+   ],
+   include_package_data=True,
    install_requires=['miricoord'],
 )
