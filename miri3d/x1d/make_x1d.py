829bde74e1b6ae3a6922de2fc22af17b7f30b856
David Law <dlaw@stsci.edu>
2020-10-30 21:13:48 -0400

    Switch to manual dict construction

--- miri3d/x1d/make_x1d.py
+++ miri3d/x1d/make_x1d.py
@@ -13,6 +13,7 @@ REVISION HISTORY:
 """
 
 import asdf
+from asdf import AsdfFile
 from astropy.io import fits
 from astropy.time import Time
 import datetime
@@ -57,16 +58,16 @@ def make_x1dpar():
     # the dictionary by hand.  We will save that routine for future
     # reference, although it can't be read in in quite the same
     # way (datamodels.open works, but results can't be called the same)
-    #ff=make_x1d_fromdict(now,cdp_dir,outplot)
+    ff=make_x1d_fromdict(now,cdp_dir,outplot)
 
     # Read datamodel from file
-    model=datamodels.IFUExtract1dModel()
+    #model=datamodels.IFUExtract1dModel()
     # Populate basic metadata
-    model=make_x1d_meta(model,now)
+    #model=make_x1d_meta(model,now)
     # Populate vectors of data
-    model=make_x1d_data(model,cdp_dir,outplot)
+    #model=make_x1d_data(model,cdp_dir,outplot)
     # Put the model into an asdf structure
-    ff = asdf.AsdfFile(model)
+    #ff = asdf.AsdfFile(model)
 
     # Add history info
     ff.add_history_entry('1D Extraction defaults')
@@ -75,7 +76,12 @@ def make_x1dpar():
     ff.add_history_entry('DATA USED: CDP-7')
     
     # Write out the file
-    ff.write_to(outfile,all_array_storage='inline')
+    ff.write_to(outfile)#,all_array_storage='inline')
+    print('Wrote file ',outfile)
+
+    # Read the file
+    #model2=datamodels.IFUExtract1dModel(outfile)
+    #pdb.set_trace()
     
 #############################
 
@@ -259,7 +265,7 @@ def make_x1d_fromdict(now,cdp_dir,outplot):
     plt.savefig(outplot)
 
     data={
-        'wavelenth': waves,
+        'wavelength': waves,
         'wavelength_units':'micron',
         'radius': radius,
         'radius_units':'arcsec',
@@ -302,15 +308,21 @@ def make_apcorrpar():
     cdp_dir=os.path.expandvars('$MIRIBOX')
     cdp_dir=os.path.join(cdp_dir,'CDP/CDP-7/MRS_APERCORR/')
 
+    # If there were not already a datamodel we'd have to construct
+    # the dictionary by hand.  We will save that routine for future
+    # reference, although it can't be read in in quite the same
+    # way (datamodels.open works, but results can't be called the same)
+    ff=make_apcorr_fromdict(now,cdp_dir,outplot)
+    
     # Read datamodel from file (not actually the right one, but close enough?)
-    model=datamodels.IFUExtract1dModel()
+    #model=datamodels.IFUExtract1dModel()
     # Populate basic metadata
-    model=make_apcorr_meta(model,now)
+    #model=make_apcorr_meta(model,now)
     # Populate vectors of data
-    model=make_apcorr_data(model,cdp_dir,outplot)
+    #model=make_apcorr_data(model,cdp_dir,outplot)
     # Put the model into an asdf structure
-    ff = asdf.AsdfFile(model)
-
+    #ff = asdf.AsdfFile(model)
+    
     # Add history info
     ff.add_history_entry('1D Extraction defaults')
     ff.add_history_entry('DOCUMENT: TBD')
@@ -422,3 +434,117 @@ def make_apcorr_data(model,cdp_dir,outplot):
     model.data.apcorr_err = aperr
     
     return model
+
+
+#############################
+
+def make_apcorr_fromdict(now,cdp_dir,outplot):
+    meta={}
+    meta['telescope']='JWST'
+    meta['pedigree']='GROUND'
+    meta['description']='Default MIRI MRS Aperture correction parameters'
+    meta['date']=now.value
+    meta['reftype']='APCORR'
+    meta['exp_type']='MIR_MRS'
+    meta['useafter']='2000-01-01T00:00:00'
+    meta['version']=int(now.mjd)
+    meta['author']='D. Law'
+    meta['origin']='STSCI'
+    meta['datamodl']='MirMrsApcorrModel'
+    meta['history']='1D Extraction defaults'
+    meta['history']+=' DOCUMENT: TBD'
+    meta['history']+=' SOFTWARE: https://github.com/STScI-MIRI/miri3d/tree/master/miri3d/x1d/make_x1d.py'
+    meta['history']+=' DATA USED: CDP-7'        
+    meta['instrument']={
+        'name': 'MIRI'
+        }
+    
+    print('Figuring out wavelength ranges')
+    wmin1A,_=mc.waveminmax('1A')
+    _,wmax4C=mc.waveminmax('4C')
+
+    print('Building tables')
+
+    # Set channel and band info
+    chan = np.array(['ANY'])
+    band = np.array(['ANY'])
+
+    # Set the output wavelength sampling for the reference file
+    waves = np.arange(wmin1A, wmax4C, 0.01,dtype='float32')
+    nwave = len(waves)
+
+    # We'll set up 3 radius options for now at each wavelength
+    # that can later be used to interpolate to different radius choices
+    nrad = 3
+    # Define placeholder arrays for radius, apcor, and aperr
+    radius = np.zeros([nrad, nwave],dtype='float32')
+    apcor = np.zeros([nrad, nwave],dtype='float32')
+    aperr = np.zeros([nrad, nwave],dtype='float32') # Placeholder, all zeros currently
+
+    # Populate real values
+    # Read in the CDP files
+    files=['MIRI_FM_MIRIFUSHORT_1SHORT_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFUSHORT_1MEDIUM_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFUSHORT_1LONG_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFUSHORT_2SHORT_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFUSHORT_2MEDIUM_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFUSHORT_2LONG_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_3SHORT_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_3MEDIUM_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_3LONG_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_4SHORT_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_4MEDIUM_APERCORR_07.00.00.fits',
+           'MIRI_FM_MIRIFULONG_4LONG_APERCORR_07.00.00.fits']
+    inwave=[]
+    inap=[]
+    incor=[]
+    for file in files:
+        hdu=fits.open(os.path.join(cdp_dir,file))
+        data=hdu[1].data
+        inwave.append(data['wavelength'])
+        inap.append(data['a_aperture'])
+        incor.append(data['aper_corr'])
+
+    # Compile into big vectors
+    # Simple polynomial fit to the aperture
+    thefit = np.polyfit(np.array(inwave).ravel(), np.array(inap).ravel(), 1)
+    poly = np.poly1d(thefit)
+    # Radius is the evaluation of this polynomial fit at the output wavelengths
+    # For now the low/high radius options are notional
+    radius[0, :] = poly(waves)/2
+    radius[1, :] = poly(waves)
+    radius[2, :] = poly(waves)*2
+
+    # At present the CDP aperture-correction factors have unphysical features
+    # Therefore do a simple linear fit to the values with wavelength
+    thefit=np.polyfit(np.array(inwave).ravel(),np.array(incor).ravel(),1)
+    poly=np.poly1d(thefit)
+    # Evaluate polynomial fit to get aperture corrections
+    # For now all radius options are the same
+    apcor[0, :] = poly(waves)/2.# TEMPORARY!!!!
+    apcor[1, :] = poly(waves)
+    apcor[2, :] = poly(waves)*2.# TEMPORARY!!!!
+
+    plt.plot(inwave,incor,'.')
+    plt.plot(waves[:],apcor[1,:])
+    plt.xlabel('Wavelength (micron)')
+    plt.ylabel('Correction factor')
+    plt.savefig(outplot)
+
+    data={
+        'wavelength': waves,
+        'wavelength_units':'micron',
+        'radius': radius,
+        'radius_units':'arcsec',
+        'apcorr': apcor,
+        'apcorr_err': aperr,
+        }
+
+    tree={
+        'meta':meta,
+        'data':data
+        }
+    
+    ff=asdf.AsdfFile(tree)
+    
+    return ff
